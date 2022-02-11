4d3477c233f4128fd7ca4a06e47998e5c87f7ad0
FabianaRSFF <fabianarsantos@gmail.com>
2022-02-11 15:02:51 -0300

    Exemplos de templates temp

---
+++ recipes/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'recipes/home.html', context={
+        'name': 'Fabiana',
+    })
+
+
+def contato(request):
+    return render(request, 'me-apague/temp.html')
+
+
+def sobre(request):
+    return HttpResponse('Sobre.')
+  
+
+
+  
+
