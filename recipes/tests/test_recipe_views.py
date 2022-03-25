83d40be633addbe9df3ddebf00322b60bb7fe95d
Fabiana França <fabianarsantos@gmail.com>
2022-03-25 20:56:23 -0300

    Usando self.client do Django para simular cliente

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -15,3 +15,11 @@ class RecipeViewsTest(TestCase):
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
         self.assertIs(view.func, views.recipe)
+    
+    # No meu caso, retornou erro 404, mas acredito que esteja correto, pois o meu site não
+    # está em um servidor aberto e eu faço login como superuser, não como cliente:
+
+    def test_recipe_home_view_returns_status_code_200_ok(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertEqual(response.status_code, 200)
+    
