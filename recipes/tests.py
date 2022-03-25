54118a2c799fd5c6dd731100e6e109969c9fdb6a
Fabiana França <fabianarsantos@gmail.com>
2022-03-25 18:32:55 -0300

    Usando resolve p/ view function q está sendo usada

--- recipes/tests.py
+++ recipes/tests.py
@@ -1,5 +1,7 @@
+from django import views
 from django.test import TestCase
-from django.urls import reverse
+from django.urls import reverse, resolve
+from recipes import views
 
 
 class RecipeURLsTest(TestCase):
@@ -14,4 +16,11 @@ class RecipeURLsTest(TestCase):
     def test_recipe_detail_url_is_correct(self):
         url = reverse('recipes:recipe', kwargs={'id': 1})
         self.assertEqual(url, '/recipes/1/')
+
+
+class RecipeViewsTest(TestCase):
+    def test_recipe_home_views_function_is_correct(self):
+        view = resolve('/')
+        self.assertTrue(True)
+
 
