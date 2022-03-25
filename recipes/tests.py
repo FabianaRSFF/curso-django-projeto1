76d21448a5d3c4082f1c1ecdbbcc0a446dcae392
Fabiana França <fabianarsantos@gmail.com>
2022-03-25 18:48:21 -0300

    Usando resolve p/ entender q view function usada

--- recipes/tests.py
+++ recipes/tests.py
@@ -19,8 +19,16 @@ class RecipeURLsTest(TestCase):
 
 
 class RecipeViewsTest(TestCase):
-    def test_recipe_home_views_function_is_correct(self):
-        view = resolve('/')
-        self.assertTrue(True)
+    def test_recipe_home_view_function_is_correct(self):
+        view = resolve(reverse('recipes:home'))
+        self.assertIs(view.func, views.home)
+
+    def test_recipe_category_view_function_is_correct(self):
+        view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
+        self.assertIs(view.func, views.category)
+
+    def test_recipe_detail_view_function_is_correct(self):
+        view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
+        self.assertIs(view.func, views.recipe)
 
 
