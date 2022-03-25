9f9569311b1a4f5f83344711f014a20e5be4094d
Fabiana França <fabianarsantos@gmail.com>
2022-03-25 16:55:09 -0300

    Testando Django Reverse URL

--- recipes/tests.py
+++ recipes/tests.py
@@ -4,7 +4,11 @@ from django.urls import reverse
 
 class RecipeURLsTest(TestCase):
     def test_recipe_home_url_is_correct(self):
-        home_url = reverse('recipes:home')
-        self.assertEqual(home_url, '/')
+        url = reverse('recipes:home')
+        self.assertEqual(url, '/')
+
+    def test_recipe_home_url_is_correct(self):
+        url = reverse('recipes:category', args=(1,))
+        self.assertEqual(url, '/')
 
         
