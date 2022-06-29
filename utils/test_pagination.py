594a8223ad400aaa20e4378a27e2677729a55c13
Fabiana <fabianarsantos@gmail.com>
2022-06-29 10:00:12 -0300

    115. Modificando o range páginas quando necessário

--- utils/test_pagination.py
+++ utils/test_pagination.py
@@ -13,4 +13,23 @@ class PaginationTest(TestCase):
         self.assertEqual([1, 2, 3, 4], pagination)
 
     def test_first_page_is_static_if_current_page_is_less_than_middle_page(self):
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=1,
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)
         
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=2,
+        )
+        self.assertEqual([1, 2, 3, 4], pagination)
+
+        pagination = make_pagination_range(
+            page_range=list(range(1, 21)),
+            qty_pages=4,
+            current_page=3,
+        )
+        self.assertEqual([2, 3, 4, 5], pagination)
