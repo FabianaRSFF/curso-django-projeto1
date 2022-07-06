b5d3594c775ea4512d21f26bf358a6915e241708
Fabiana <fabianarsantos@gmail.com>
2022-07-06 14:14:05 -0300

    Enviando POST para outra URL usando SESSION/salvar

--- authors/views.py
+++ authors/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from tkinter.messagebox import NO
+from django.shortcuts import redirect, render
 from .forms import RegisterForm
+from django.http import Http404
 
 
 def register_view(request):
-    if request.POST:
-        form = RegisterForm(request.POST)
-    else:
-        form = RegisterForm()
+    register_form_data = request.session.get('register_form_data', None)
+    request.session['number'] = request.session.get('number') or 1
+    request.session['number'] += 1
+    form = RegisterForm(register_form_data)
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
     })
+
+
+def register_create(request):
+    if not request.POST:
+        raise Http404()
+
+    POST = request.POST
+    request.session['register_form_data'] = POST
+    form = RegisterForm(POST)
+    
+    return redirect('authors:register')
