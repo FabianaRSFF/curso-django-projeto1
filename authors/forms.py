c4b893b88521640d9327d13a94460ff8fbb0daf2
Fabiana <fabianarsantos@gmail.com>
2022-07-07 17:20:03 -0300

    Sobrescrevendo campos do formulário completamente

--- authors/forms.py
+++ authors/forms.py
@@ -1,8 +1,50 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.forms import widgets
+
+
+def add_attr(field, attr_name, attr_new_val):
+    existing_attr = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing_attr} {attr_new_val}'.strip()
+
+
+def add_placeholder(field, placeholder_val):
+    field.widget.attrs['placeholder'] = placeholder_val
 
 
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        add_placeholder(self.fields['username'], 'Your username.')
+        add_placeholder(self.fields['email'], 'Your e-mail.')
+        add_placeholder(self.fields['first_name'], 'Ex.: James')
+        add_placeholder(self.fields['last_name'], 'Ex.: Bond')
+
+    password = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Type your password.'
+        }),
+        error_messages={
+                'required': 'This field must not be empty.'
+        },
+        help_text=(
+            'Password must have at least one uppercase letter,'
+            ' one lowercase letter and one number. At least '
+            '8 characters.'
+        )
+        
+    )
+    password2 = forms.CharField(
+        required=True,
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Repeat your password.'
+        }),
+        error_messages={
+                'required': 'This field must not be empty.'
+            }
+    )
+
     class Meta:
         model = User
         fields = [
