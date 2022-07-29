ff6e07cc77796758c796c6866e16303fb2d05d5e
Fabiana <fabianarsantos@gmail.com>
2022-07-29 18:40:36 -0300

    grouping functional tests with pytest markers

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,7 +1,10 @@
 from selenium.webdriver.common.by import By
 from .base import RecipeBaseFunctionalTest
+import pytest
 
 
+
+@pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
     def test_recipe_home_page_recipes_error_message(self):
         self.browser.get(self.live_server_url)
