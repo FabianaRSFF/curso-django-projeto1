816bbabb2ba0fcf009dcab670f70c61266f21c22
Fabiana <fabianarsantos@gmail.com>
2022-07-30 17:46:35 -0300

    Functional test for pagination with accessibility

--- recipes/tests/test_recipe_base.py
+++ recipes/tests/test_recipe_base.py
@@ -62,13 +62,12 @@ class RecipeMixin:
         recipes = []
         for i in range(qtd):
             kwargs = {
-                'title': 'Recipe Title {i}',
+                'title': f'Recipe Title {i}',
                 'slug': f'r{i}',
                 'author_data': {'username': f'u{i}'}}
             recipe = self.make_recipe(**kwargs)
             recipes.append(recipe)
         return recipes
-
 
 
 class RecipeTestBase(TestCase, RecipeMixin):
