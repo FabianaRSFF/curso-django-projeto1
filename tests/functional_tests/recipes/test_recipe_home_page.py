f5df8d5baa6de8e4133cdf49edec32d04adc154c
Fabiana <fabianarsantos@gmail.com>
2022-07-30 17:00:19 -0300

    Finding Inputs, texts & keys with Selenium

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from .base import RecipeBaseFunctionalTest
 import pytest
 from unittest.mock import patch
@@ -11,4 +12,31 @@ class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
         self.browser.get(self.live_server_url)
         body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('No recipes found here.', body.text)
+        
+    @patch('recipes.views.PER_PAGE', new=2)
+    def test_recipe_search_input_can_find_correct_recipes(self):
+        recipes = self.make_recipe_in_batch()
+
+        title_needed = 'This is what I need'
+        recipes[0].title = title_needed
+        recipes[0].save()
+
+        self.browser.get(self.live_server_url)
+        search_input = self.browser.find_element(
+            By.XPATH,
+            '//input[@placeholder="Search for a recipe"]'
+        )
+        search_input.send_keys(title_needed)
+        search_input.send_keys(Keys.ENTER)
+
+        self.assertIn(
+            title_needed,
+            self.browser.find_element(By.TAG_NAME, 'body').text,   
+        )
+        self.assertIn(
+           title_needed,
+           self.browser.find_element(By.CLASS_NAME, 'main-content-list').text, 
+        )
+        self.sleep(6)
+
         
