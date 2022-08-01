8966fc4dd6b290a0f91600ffe956f4da13c4d0a7
Fabiana <fabianarsantos@gmail.com>
2022-08-01 09:20:28 -0300

    Testing User & Password

--- tests/functional_tests/authors/test_authors_login.py
+++ tests/functional_tests/authors/test_authors_login.py
@@ -58,3 +58,23 @@ class AuthorsLoginTest(AuthorsBaseTest):
                 'Invalid username or password.',
                 self.browser.find_element(By.TAG_NAME, 'body').text
             )
+
+    def test_form_login_invalid_credentials(self):
+        def callable(form):
+            self.browser.get(
+                self.live_server_url + reverse('authors:login')
+            )
+
+            form = self.browser.find_element(By.CLASS_NAME, 'main-form')
+
+            username = self.get_by_placeholder(form, 'Your username.')
+            password = self.get_by_placeholder(form, 'Type your password here')
+            username.send_keys('invalid_user')
+            password.send_keys('invalid_password')
+
+            form.submit()
+
+            self.assrtIn(
+                'Invalid credentials.',
+                self.browser.find_element(By.TAG_NAME, 'body').text
+            )
