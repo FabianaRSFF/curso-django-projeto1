22bb1a9aa405e2d6fab2b34b6346eff6509b259e
Fabiana <fabianarsantos@gmail.com>
2023-04-09 19:01:19 -0300

    renaming fields/using response methods to serialize in DRF

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -5,4 +5,9 @@ class RecipeSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     description = serializers.CharField(max_length=165)
     title = serializers.CharField(max_length=65)
+    public = serializers.BooleanField(source='is_published')
+    preparation = serializers.SerializerMethodField()
+    
+    def get_preparation(self, recipe):
+        return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
  
