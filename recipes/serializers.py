79eb2a9d622acf9b5dab50230a8a904bda2c273a
Fabiana <fabianarsantos@gmail.com>
2023-04-10 16:03:20 -0300

    fields related with DRF

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,4 +1,12 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
+from tag.models import Tag
+
+
+class TagSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField(max_length=255)
+    slug = serializers.SlugField()
 
 
 class RecipeSerializer(serializers.Serializer):
@@ -7,7 +15,19 @@ class RecipeSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=65)
     public = serializers.BooleanField(source='is_published')
     preparation = serializers.SerializerMethodField()
-    
+    category = serializers.StringRelatedField()
+    author = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all()
+    )
+    tags = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.all(),
+        many=True,
+    )
+    tag_objects = TagSerializer(
+        many=True, source='tags'
+    )
+
     def get_preparation(self, recipe):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
- 
+
+   
