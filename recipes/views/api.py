af038663719e36a6bd0538a7160ee9e2d6ab8d46
Fabiana <fabianarsantos@gmail.com>
2023-04-12 15:32:53 -0300

    HyperlinkedRelatedField

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -3,12 +3,18 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from ..models import Recipe
 from ..serializers import RecipeSerializer
+from tag.models import Tag
+from ..serializers import TagSerializer
 
 
 @api_view()
 def recipe_api_list(request):
     recipes = Recipe.objects.get_published()[:10]
-    serializer = RecipeSerializer(instance=recipes, many=True)
+    serializer = RecipeSerializer(
+        instance=recipes,
+        many=True,
+        context={'request': request},
+        )
     return Response(serializer.data)
 
 
@@ -18,5 +24,25 @@ def recipe_api_detail(request, pk):
         Recipe.objects.get_published(),
         pk=pk
     )
-    serializer = RecipeSerializer(instance=recipe, many=False)
+    serializer = RecipeSerializer(
+        instance=recipe,
+        many=False,
+        context={'request': request},
+        )
     return Response(serializer.data)
+
+
+@api_view()
+def tag_api_detail(request, pk):
+    tag = get_object_or_404(
+        Tag.objects.all(),
+        pk=pk
+    )
+    serializer = TagSerializer(
+        instance=tag,
+        many=False,
+        context={'request': request},
+        )
+    return Response(serializer.data)
+    
+  
