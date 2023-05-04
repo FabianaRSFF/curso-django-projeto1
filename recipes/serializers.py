8330379a5673b1f3ebd387fa698f5d7b1d65068d
Fabiana França <fabianarsantos@gmail.com>
2023-05-04 01:16:39 +0000

    Router, urlpatterns, path & include in DRF

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -9,40 +9,55 @@ from .models import Recipe
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
-        fields = ['id', 'name', 'slug']
+        fields = ["id", "name", "slug"]
 
 
 class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = [
-            'id', 'title', 'description', 'author',
-            'category', 'tags', 'public', 'preparation',
-            'tag_objects', 'tag_links',
-            'preparation_time', 'preparation_time_unit', 'servings',
-            'servings_unit',
-            'preparation_steps', 'cover'
+            "id",
+            "title",
+            "description",
+            "author",
+            "category",
+            "tags",
+            "public",
+            "preparation",
+            "tag_objects",
+            "tag_links",
+            "preparation_time",
+            "preparation_time_unit",
+            "servings",
+            "servings_unit",
+            "preparation_steps",
+            "cover",
         ]
+
     id = serializers.IntegerField()
     description = serializers.CharField(max_length=165)
     title = serializers.CharField(max_length=65)
-    public = serializers.BooleanField(source='is_published', read_only=True)
+    public = serializers.BooleanField(source="is_published", read_only=True)
     preparation = serializers.SerializerMethodField(read_only=True)
     category = serializers.StringRelatedField(read_only=True)
     tag_objects = TagSerializer(
-        many=True, source='tags', read_only=True,
+        many=True,
+        source="tags",
+        read_only=True,
     )
     tag_links = serializers.HyperlinkedRelatedField(
-        many=True,
-        source='tags',
-        read_only=True,
-        view_name='recipes:recipe_api_v2_tag'
+        many=True, source="tags", read_only=True, view_name="recipes:recipe_api_v2_tag"
     )
 
     def get_preparation(self, recipe):
-        return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
+        return f"{recipe.preparation_time} {recipe.preparation_time_unit}"
 
     def validate(self, attrs):
+        if self.instance is not None and attrs.get("servings") is None:
+            attrs["servings"] = self.instance.servings
+        if self.instance is not None and attrs.get("preparation_time") is None:
+            attrs["preparation_time"] = self.instance.preparation_time
+
         super_validate = super().validate(attrs)
 
         AuthorRecipeValidator(
