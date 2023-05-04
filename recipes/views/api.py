9df54c0c9b5d22ea9496fef8002f92cd4245d846
Fabiana França <fabianarsantos@gmail.com>
2023-05-04 01:17:44 +0000

    Router, urlpatterns, path & include in DRF

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,8 +1,5 @@
 from django.shortcuts import get_object_or_404
-from rest_framework import status
 from rest_framework.decorators import api_view
-from rest_framework.generics import (ListCreateAPIView,
-                                     RetrieveUpdateDestroyAPIView)
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
@@ -22,14 +19,14 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
 
-    def patch(self, request, *args, **kwargs):
-        pk = kwargs.get('pk')
+    def partial_update(self, request, *args, **kwargs):
+        pk = kwargs.get("pk")
         recipe = self.get_queryset().filter(pk=pk).first()
         serializer = RecipeSerializer(
             instance=recipe,
             data=request.data,
             many=False,
-            context={'request': request},
+            context={"request": request},
             partial=True,
         )
         serializer.is_valid(raise_exception=True)
@@ -41,13 +38,10 @@ class RecipeAPIv2ViewSet(ModelViewSet):
 
 @api_view()
 def tag_api_detail(request, pk):
-    tag = get_object_or_404(
-        Tag.objects.all(),
-        pk=pk
-    )
+    tag = get_object_or_404(Tag.objects.all(), pk=pk)
     serializer = TagSerializer(
         instance=tag,
         many=False,
-        context={'request': request},
+        context={"request": request},
     )
     return Response(serializer.data)
