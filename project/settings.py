837d8018ee15222fa4b26bc3be5c100aa2e5929c
Fabiana França <fabianarsantos@gmail.com>
2023-05-05 18:18:34 +0000

    updating

--- project/settings.py
+++ project/settings.py
@@ -15,8 +15,9 @@ from pathlib import Path
 
 from django.contrib.messages import constants
 
-if os.environ.get('DEBUG', None) is None:
+if os.environ.get("DEBUG", None) is None:
     from dotenv import load_dotenv
+
     load_dotenv()
 
 
@@ -30,84 +31,84 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get('SECRET_KEY', 'INSECURE')
+SECRET_KEY = os.environ.get("SECRET_KEY", "INSECURE")
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = True if os.environ.get('DEBUG') == '1' else False
+DEBUG = True if os.environ.get("DEBUG") == "1" else False
 
 
 # TODO  Change this with CSRF_TRUSTED_ORIGINS before deploy:
-ALLOWED_HOSTS: list[str] = ['*']
+ALLOWED_HOSTS: list[str] = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'rest_framework',
-    'recipes',
-    'authors',
-    'tag',
-    'debug_toolbar',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "rest_framework",
+    "recipes",
+    "authors",
+    "tag",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'project.urls'
+ROOT_URLCONF = "project.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / 'base_templates',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            BASE_DIR / "base_templates",
         ],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'project.wsgi.application'
+WSGI_APPLICATION = "project.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
 DATABASES = {
-    'default': {
-        'ENGINE': os.environ.get('DATABASE_ENGINE'),
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
-        'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': os.environ.get('DATABASE_PORT'),
+    "default": {
+        "ENGINE": os.environ.get("DATABASE_ENGINE"),
+        "NAME": os.environ.get("DATABASE_NAME"),
+        "USER": os.environ.get("DATABASE_USER"),
+        "PASSWORD": os.environ.get("DATABASE_PASSWORD"),
+        "HOST": os.environ.get("DATABASE_HOST"),
+        "PORT": os.environ.get("DATABASE_PORT"),
     }
 }
 
@@ -116,16 +117,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa: E501
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",  # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',  # noqa: E501
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",  # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',  # noqa: E501
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",  # noqa: E501
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',   # noqa: E501
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",  # noqa: E501
     },
 ]
 
@@ -133,46 +134,48 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'pt-BR'
+LANGUAGE_CODE = "pt-BR"
 
-TIME_ZONE = 'America/Sao_Paulo'
+TIME_ZONE = "America/Sao_Paulo"
 
-USE_I18N = True
+USE_I18N = False
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    BASE_DIR / 'base_static',
+    BASE_DIR / "base_static",
 ]
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / "static"
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MESSAGE_TAGS = {
-    constants.DEBUG: 'message-debug',
-    constants.ERROR: 'message-error',
-    constants.INFO: 'message-info',
-    constants.SUCCESS: 'message-success',
-    constants.WARNING: 'message-warning',
+    constants.DEBUG: "message-debug",
+    constants.ERROR: "message-error",
+    constants.INFO: "message-info",
+    constants.SUCCESS: "message-success",
+    constants.WARNING: "message-warning",
 }
 
 # Django Debug Toolbar
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
+INTERNAL_IPS = ["127.0.0.1"]
+
 
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10,
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.LimitOffsetPagination",  # noqa E501
+    "PAGE_SIZE": 10,
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
 }
--- recipes/urls.py
+++ recipes/urls.py
@@ -1,33 +1,55 @@
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import SimpleRouter
+
 from recipes import views
 
-app_name = 'recipes'
+# TokenRefreshView,
+# TokenVerifyView,
+
+
+app_name = "recipes"
 
 recipe_api_v2_router = SimpleRouter()
 recipe_api_v2_router.register(
-    'recipes/api/v2', 
-    views.RecipeAPIv2ViewSet,
-    basename='recipes-api'
+    "recipes/api/v2", views.RecipeAPIv2ViewSet, basename="recipes-api"
 )
 
 
 urlpatterns = [
-    path('', views.RecipeListViewHome.as_view(), name="home"),
-    path('recipes/search/', views.RecipeListViewSearch.as_view(), name='search'),   # noqa E501
-    path('recipes/tags/<slug:slug>', views.RecipeListViewTag.as_view(), name='tag'),   # noqa E501
-    path('recipes/category/<int:category_id>/', views.RecipeListViewCategory.as_view(),  # noqa E501
-         name='category'),
-    path('recipes/<int:pk>/', views.RecipeDetail.as_view(), name='recipe'),
-    path('recipes/api/v1/', views.RecipeListViewHomeAPI.as_view(), name='recipes_api_v1'),  # noqa E501
-    path('recipes/api/v1/<int:pk>/', views.RecipeDetailAPI.as_view(), name='recipes_api_v1_detail'),  # noqa E501
-    path('recipes/theory', views.theory, name='theory',),
+    path("", views.RecipeListViewHome.as_view(), name="home"),
     path(
-        'recipes/api/v2/tag/<int:pk>/',
+        "recipes/search/", views.RecipeListViewSearch.as_view(), name="search"
+    ),  # noqa E501
+    path(
+        "recipes/tags/<slug:slug>",
+        views.RecipeListViewTag.as_view(),
+        name="tag",  # noqa
+    ),  # noqa E501
+    path(
+        "recipes/category/<int:category_id>/",
+        views.RecipeListViewCategory.as_view(),  # noqa E501
+        name="category",
+    ),
+    path("recipes/<int:pk>/", views.RecipeDetail.as_view(), name="recipe"),
+    path(
+        "recipes/api/v1/",
+        views.RecipeListViewHomeAPI.as_view(),
+        name="recipes_api_v1",  # noqa
+    ),  # noqa E501
+    path(
+        "recipes/api/v1/<int:pk>/",
+        views.RecipeDetailAPI.as_view(),
+        name="recipes_api_v1_detail",
+    ),  # noqa E501
+    path(
+        "recipes/theory",
+        views.theory,
+        name="theory",
+    ),
+    path(
+        "recipes/api/v2/tag/<int:pk>/",
         views.tag_api_detail,
-        name='recipe_api_v2_tag'
-        ),
-    
-    path('', include(recipe_api_v2_router.urls))
-]
-
+        name="recipe_api_v2_tag",  # noqa E501
+    ),
+    path("", include(recipe_api_v2_router.urls)),
+]  # noqa E501
